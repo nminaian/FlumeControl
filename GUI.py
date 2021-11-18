2e3115ec92e7548d4970d85ac20c6eeb640aa133
Devin Krystek <devkmtt@gmail.com>
2021-11-17 18:11:44 -0800

    Implemented some small improvements

--- GUI.py
+++ GUI.py
@@ -1,7 +1,13 @@
 import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 
-sg.theme("DarkAmber")
+# sg.theme_previewer()
+# HotDogStand = meme
+# DarkGreen5 is good
+# can use "sg.theme_previewer()"" to look at all the themes available
+sg.theme("HotDogStand")
+
+# TODO look at the "Animated Matplotlib" in the cookbook and attempt to implement the live graph reading
 
 data = []
 color = "b"
@@ -18,7 +24,18 @@ color = "b"
 
 layout = [
     [sg.Text("Some text on row 1")],
-    [sg.Slider((0, 100), default_value=50, orientation="horizontal")],
+    [
+        sg.Slider(
+            (0, 100),
+            key="-slider-",
+            default_value=50,
+            orientation="horizontal",
+            expand_x=True,
+            enable_events=True,
+            disable_number_display=True,
+        ),
+        sg.T("", size=(4, 1)),
+    ],
     # [sg.Text("Enter something on row 2"), sg.InputText()],
     [sg.Graph((100, 100), (0, 0), (100, 100), background_color="white")],
     [
@@ -29,12 +46,15 @@ layout = [
         sg.Button("Close"),
     ],
 ]
-
-window = sg.Window("Window Title", layout)
-while True:
+window = sg.Window("Flume Control", layout)
+print("test1")
+while True:  # Event Loop
     event, values = window.read()
-    data.append(values[0])
-    if event == sg.WIN_CLOSED or event == "Cancel":
+    print("test2")
+    data.append(values["-slider-"])
+    plt.plot(data, color)
+    plt.show()
+    if event == sg.WIN_CLOSED or event == "Close":
         break
     elif event == "Clear":
         data = []
@@ -47,10 +67,10 @@ while True:
         f.write(data)
         print(f.read())
     elif event == "Ok":
-        print("You entered", values[0])
+        data.append(values["-slider-"])
+        print("You entered", values["-slider-"])
         print("data = ", data)
         print("values= ", values)
         plt.plot(data, color)
         plt.show(block=False)
-
 window.close()
